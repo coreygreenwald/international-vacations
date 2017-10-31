6aeb99bf8d842470ed9917598180bfe5d4ebee0f
Corey Greenwald <coreygreen1108@gmail.com>
2017-10-31 11:19:20 -0400

    initial set up of server and module installation, static serve html

---
+++ main.js
@@ -0,0 +1,5 @@
+const app = require('./server');
+
+app.listen(8080, () => {
+    console.log('server running on PORT 8080!');
+})
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+
+//logging middleware
+app.use(volleyball);
+
+//body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//static middleware
+app.use(express.static(path.join(__dirname, '../public')));
+
+const apiRoutes = require('./api');
+app.use('/api', apiRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
+
+module.exports = app;
