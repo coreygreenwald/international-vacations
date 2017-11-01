d922b7447d1f5f5fd270a66ed14c141cef67bfd1
Corey Greenwald <coreygreen1108@gmail.com>
2017-11-01 17:53:03 -0400

    added basic front end template

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './client/index.js',
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    }
+  };
