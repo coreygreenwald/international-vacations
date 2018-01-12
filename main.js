0c73db70bc882ad9fbd62af48b1a5c243472d3a5
Corey Greenwald <coreygreen1108@gmail.com>
2018-01-12 12:52:58 -0500

    process env controls port

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const db = require('./db').db;
 
 db.sync()
 .then(() => {
-    app.listen(5000, () => {
+    app.listen(process.env.PORT || 5000, () => {
         console.log('server running on PORT 5000!');
     })
 })
