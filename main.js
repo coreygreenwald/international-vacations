311365a9308c7153e71a2df98d2508fc2abda55f
Corey Greenwald <coreygreen1108@gmail.com>
2018-01-12 12:37:10 -0500

    add bundle js

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const app = require('./server');
 const db = require('./db').db;
-const seeder = require('./seed');
 
 db.sync({force: true})
-.then(() => seeder())
 .then(() => {
     app.listen(5000, () => {
         console.log('server running on PORT 5000!');
