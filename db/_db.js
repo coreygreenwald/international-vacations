22650350395feb36565b028288b741801d84760c
Corey Greenwald <coreygreen1108@gmail.com>
2018-01-12 12:47:42 -0500

    allow db config from heroku

--- db/_db.js
+++ db/_db.js
@@ -5,10 +5,12 @@ const path      = require('path');
 const Sequelize = require('sequelize');
 const basename  = path.basename(__filename);
 const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/../config/config.json')[env];
+const config    = process.env.DATABASE_URL || require(__dirname + '/../config/config.json')[env];
 
 let db;
-if (config.use_env_variable) {
+if(process.env.DATABASE_URL){
+    db = new Sequelize(process.env.DATABASE_URL, {logging: false}); 
+} else if (config.use_env_variable) {
     db = new Sequelize(process.env[config.use_env_variable]);
 } else {
     db = new Sequelize(config.database, config.username, config.password, config);
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const app = require('./server');
 const db = require('./db').db;
 
-db.sync({force: true})
+db.sync()
 .then(() => {
     app.listen(5000, () => {
         console.log('server running on PORT 5000!');
